395901d8af7d67362d551ab1a9d7879bd129214c
hammit-01 <mpa0530@gmail.com>
2026-04-15 13:55:28 +0900

    web/app update

--- 재고장.py
+++ 재고장.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="재고 시스템")
 
-# User-Agent 감지
 try:
     user_agent = st.context.headers.get("User-Agent", "")
 except:
@@ -10,15 +9,18 @@ except:
 
 is_mobile = any(k in user_agent for k in ["Mobile", "Android", "iPhone"])
 
-# 모드 선택 (기본값 자동)
 mode = st.sidebar.selectbox("모드 선택", ["자동", "웹", "모바일"])
 
-# 분기 처리
+st.title("📦 재고 시스템")
+
 if mode == "자동":
-    if is_mobile:
-        st.switch_page("pages/mobile.py")
-    else:
-        st.switch_page("pages/web.py")
+    st.write("자동 모드:", "📱 모바일" if is_mobile else "💻 웹")
+    
+    if st.button("페이지 이동"):
+        if is_mobile:
+            st.switch_page("pages/mobile.py")
+        else:
+            st.switch_page("pages/web.py")
 
 elif mode == "웹":
     st.switch_page("pages/web.py")
